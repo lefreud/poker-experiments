c1c0c4be27f22d3ffa263e443d25c736d1f24e50
lefreud <freud.fortier.chouinard@gmail.com>
2020-06-26 03:07:44 +0000

    Added equilibrium computation

--- rps_regret_matching/main.py
+++ rps_regret_matching/main.py
@@ -3,45 +3,35 @@ from random import random
 
 class RPSTrainer:
     NUM_ACTIONS = 3
+    NUM_PLAYERS = 2
 
-    def __init__(self, *, iterations, opponent_strategy):
-        self.__cumulative_strategy = [[0, 0,0],[0,0,0]]
-        self.__opponent_strategy = opponent_strategy
+    def __init__(self, *, iterations):
+        self.__cumulative_strategy = [[0,0,0],[0,0,0]]
+        self.__cumulative_regrets = [[0,0,0],[0,0,0]]
         self.__iterations = iterations
 
     def train(self):
         for _ in range(self.__iterations):
-            hero_action = self.get_hero_action()
-            opponent_action = self.get_opponent_action()
-            actionUtility = [0] * RPSTrainer.NUM_ACTIONS
-            actionUtility[opponent_action] = 0
-            actionUtility[(opponent_action + 1) % RPSTrainer.NUM_ACTIONS] = 1
-            actionUtility[(opponent_action + 2) % RPSTrainer.NUM_ACTIONS] = -1
-            for a in range(RPSTrainer.NUM_ACTIONS):
-                self.__cumulative_regrets[a] += actionUtility[a] - actionUtility[hero_action]
+            strategies = [self.get_strategy(0), self.get_strategy(1)]
+            for p in range(RPSTrainer.NUM_PLAYERS):
+                for a in range(RPSTrainer.NUM_ACTIONS):
+                    self.__cumulative_strategy[p][a] = self.__cumulative_strategy[p][a] + strategies[p][a]
+            
+            simultaneous_actions = [self.sample_action(strategies[0]), self.sample_action(strategies[1])]
+            for p in range(RPSTrainer.NUM_PLAYERS):
+                hero_action = simultaneous_actions[p]
+                opponent_action = simultaneous_actions[1 - p]
+                actionUtility = [0, 0, 0]
+                actionUtility[opponent_action] = 0
+                actionUtility[(opponent_action + 1) % RPSTrainer.NUM_ACTIONS] = 1
+                actionUtility[(opponent_action + 2) % RPSTrainer.NUM_ACTIONS] = -1
+                for a in range(RPSTrainer.NUM_ACTIONS):
+                    self.__cumulative_regrets[p][a] = self.__cumulative_regrets[p][a] + actionUtility[a] - actionUtility[hero_action]
     
     def get_cumulative_regrets(self):
         return self.__cumulative_regrets
 
-    def get_hero_strategy(self):
-        normalizing_sum = sum(filter(lambda x: x > 0, self.__cumulative_regrets))
-        if normalizing_sum > 0:
-            return list(map((lambda x: x / normalizing_sum if x > 0 else 0), self.__cumulative_regrets))
-        else:
-            return [1 / RPSTrainer.NUM_ACTIONS] * RPSTrainer.NUM_ACTIONS
-
-    def get_opponent_strategy(self):
-        return self.__opponent_strategy
-
-    def get_hero_action(self):
-        return RPSTrainer.sample_random_action(self.get_hero_strategy())
-            
-
-    def get_opponent_action(self):
-        return RPSTrainer.sample_random_action(self.get_opponent_strategy())
-
-    @staticmethod
-    def sample_random_action(strategy):
+    def sample_action(self, strategy):
         r = random()
         cumulative_probability = 0
         for a in range(RPSTrainer.NUM_ACTIONS):
@@ -50,15 +40,31 @@ class RPSTrainer:
                 return a
         raise RuntimeError("No action chosen")
 
+    def get_strategy(self, player):
+        normalizing_sum = sum(filter(lambda x: x > 0, self.__cumulative_regrets[player]))
+        if normalizing_sum > 0:
+            return list(map((lambda x: x / normalizing_sum if x > 0 else 0), self.__cumulative_regrets[player]))
+        else:
+            return [1 / RPSTrainer.NUM_ACTIONS, 1 / RPSTrainer.NUM_ACTIONS, 1 / RPSTrainer.NUM_ACTIONS]
+    
+    def get_average_strategy(self, player):
+        normalizing_sum = sum(self.__cumulative_strategy[player])
+        if normalizing_sum > 0:
+            return list(map((lambda x: x / normalizing_sum), self.__cumulative_strategy[player]))
+        else:
+            return [1 / RPSTrainer.NUM_ACTIONS, 1 / RPSTrainer.NUM_ACTIONS, 1 / RPSTrainer.NUM_ACTIONS]
+
+
 def get_formatted_strategy(strategy):
     return f"Rock: {strategy[0]*100} %, Paper: {strategy[1]*100} %, Scissors: {strategy[2]*100} %"
 
 if __name__ == "__main__":
-    opponent_strategy = [0.3, 0.3, 0.4]
-    print("Opponent's strategy:")
-    print(get_formatted_strategy(opponent_strategy))
     training_iterations = 10000
-    trainer = RPSTrainer(iterations=training_iterations, opponent_strategy=opponent_strategy)
+    trainer = RPSTrainer(iterations=training_iterations)
     trainer.train()
+
     print(f"Hero strategy after {training_iterations} iterations:")
-    print(get_formatted_strategy(trainer.get_hero_strategy()))
+    print(get_formatted_strategy(trainer.get_average_strategy(0)))
+
+    print(f"Opponent strategy after {training_iterations} iterations:")
+    print(get_formatted_strategy(trainer.get_average_strategy(1)))
